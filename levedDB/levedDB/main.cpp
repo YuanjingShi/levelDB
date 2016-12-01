c5f5c419de1ebaff7cbe8788d0301f5be1698f24
ShingJan <yuanj.shi@hotmail.com>
2016-12-01 15:05:50 +0800

    leveled source code & try db_bench out

--- levedDB/levedDB/main.cpp
+++ levedDB/levedDB/main.cpp
@@ -12,38 +12,17 @@
 //it is supposed to be some comments
 int main(int intargc, char * argv[])
 {
-    leveldb::DB *db;
+    leveldb::DB* db;
     leveldb::Options options;
-    
     options.create_if_missing = true;
+    std::string dbpath = "testdb";
+    leveldb::Status status = leveldb::DB::Open (options, dbpath, &db);
+    assert (status.ok ());
+    std::string key = "test";
+    std::string val = "test_value";
+    s = db->Put (leveldb::WriteOptions (), key, val);
+    val.clear ();
+    s = db->Get (leveldb::ReadOptions (), key, &val);
     
-    std::string dbpath = "blog";
-    
-    leveldb::Status status = leveldb::DB::Open( options, dbpath,&db );
-    
-    assert ( status.ok() );
-    
-    std::string email_key = "email:jankuo";
-    std::string email_value = "llh110220@gmail.com";
-    
-    std::cout << "open db ok! " << std::endl;
-    
-    std::string value;
-    leveldb::Status sta;
-    
-    sta = db->Put( leveldb::WriteOptions(), email_key, email_value );
-    if ( sta.ok() )
-    {
-        std::cout << "put email_key" << email_key << " ok " << std::endl;
-    }
-    
-    sta = db->Get( leveldb::ReadOptions(), email_key, &value );
-    if ( sta.ok() )
-    {
-        std::cout << "get email_key:" << email_key << " ok " << std::endl;
-        std::cout << email_key << ":" << value << std::endl;
-    }
-    
-    delete db;
-    return 0;
+    std::cout << key << ": " << val << std::endl;
 }
